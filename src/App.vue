8cf62ed671bd2d4808abcdf12d29b6a3fcc994a5
KokoroKawakami <>
2020-09-29 18:27:35 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1 class="test__test">
+      Vue Samle
+    </h1>
+    <CoverComponents />
+  </div>
+</template>
+
+<script lang="ts">
+import CoverComponents from '@/components/CoverComponents'
+
+export default {
+  components: {
+    CoverComponents,
+  },
+}
+</script>
+
+<style lang="sass" scoped>
+.test
+  &__test
+    color: red
+</style>
---
+++ src/components/CoverComponents.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="test__test">
+    test components
+    <div class="test__test">
+      test components
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {}
+</script>
+
+<style lang="sass" scoped>
+.test
+  &__test
+    color: blue
+</style>
